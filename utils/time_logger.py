2ba1ace3ba54fbc4bcf5981a26fe34e355c2fd2e
andy0000-droid <andy020207@gmail.com>
2025-08-06 18:48:55 +0900

    [UPDATE] Refactoring time logging code

--- utils/time_logger.py
+++ utils/time_logger.py
@@ -23,24 +23,45 @@ except ImportError as e:
     raise e
 
 
-def time_logger(original_function):
-    """Decorator to log the execution time of a function."""
+class TimeLogger:
+    """Class to log the execution time of methods."""
 
-    def wrapper(*args, **kwargs):
-        process_start_time = time.process_time()
-        performance_start_time = time.perf_counter()
+    def __init__(self, org_func):
+        self.org_func = org_func
+        self.execution_time = None
+        self.performance_time = None
 
-        result = original_function(*args, **kwargs)
+    def __call__(self, *args, **kwargs):
+        process_start_time = time.process_time_ns()
+        performance_start_time = time.perf_counter_ns()
 
-        process_end_time = time.process_time()
-        performance_end_time = time.perf_counter()
+        result = self.org_func(*args, **kwargs)
+
+        process_end_time = time.process_time_ns()
+        performance_end_time = time.perf_counter_ns()
         execution_time = process_end_time - process_start_time
         performance_time = performance_end_time - performance_start_time
-        print(f"Execution time: {execution_time:.6f} seconds", end=", ")
-        print(f"Performance time: {performance_time:.6f} seconds")
+        print(f"Execution time: {execution_time} nano seconds", end=", ")
+        print(f"Performance time: {performance_time} nano seconds")
         send_message(
-            f"Execution time: {execution_time:.6f} seconds, Performance time: {performance_time:.6f} seconds"
+            f"Execution time: {execution_time} nano seconds, Performance time: {performance_time} nano seconds"
         )
+        self.execution_time = execution_time
+        self.performance_time = performance_time
         return result
 
-    return wrapper
+    def get_calc_time(self):
+        """Calculates the execution time and performance time."""
+        if self.execution_time is None or self.performance_time is None:
+            raise ValueError("Execution time and performance time are not set.")
+        return self.execution_time, self.performance_time
+
+
+if __name__ == "__main__":
+    # Example usage
+    @TimeLogger
+    def example_function():
+        """An example function to demonstrate the TimeLogger decorator."""
+        time.sleep(1)  # Simulate a function that takes some time to execute
+
+    example_function()
